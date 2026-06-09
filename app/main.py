13dd1cd198b9fd5532b4b765ddc0e8a0e15ec504
Alexc0dex <mirandavivancoalexander@gmail.com>
2026-06-09 17:25:32 -0500

    feat: save images sent via API in a data folder

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from datetime import datetime
+import os
+
+from fastapi import FastAPI, File, Form, UploadFile, HTTPException
 from model import FingerCountModel
 from app import PredictionResponse
 
 app = FastAPI()
+
+DATASET_DIR = "./data/dataset_nao"
+
+if not os.path.exists(DATASET_DIR):
+    os.makedirs(DATASET_DIR, exist_ok=True)
 
 model = FingerCountModel()
 
@@ -11,13 +19,27 @@ def health_check():
     return {"status": "ok", "message": "API is healthy"}
 
 @app.post("/predict", response_model=PredictionResponse)
-async def predict(file: UploadFile = File(...)):
+async def predict(file: UploadFile = File(...), etiqueta_real: int = Form(...)):
     if file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
 
     try:
         image_bytes = await file.read()
+
+        subcarpeta = os.path.join(DATASET_DIR, str(etiqueta_real))
+        if not os.path.exists(subcarpeta):
+            os.makedirs(subcarpeta, exist_ok=True)
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+        
         fingers_count = model.predict(image_bytes)
-        return PredictionResponse(fingers_count=fingers_count, message="Prediction successful")
+
+        nombre_archivo = f"{fingers_count}_img_{timestamp}.jpg"
+        ruta_completa = os.path.join(subcarpeta, nombre_archivo)
+        
+        with open(ruta_completa, "wb") as f:
+            f.write(image_bytes)
+
+        return PredictionResponse(fingers_count=fingers_count, message="Prediction successful", saved_path=ruta_completa)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel
 class PredictionResponse(BaseModel):
     fingers_count: int
     message: str
+    saved_path: str = None
